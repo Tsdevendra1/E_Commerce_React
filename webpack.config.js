0dcb1681a37bcb451cf8f98da2f01616b184ffba
tsdev <tsdevendra1@gmail.com>
2019-03-30 10:14:11 +0000

    - Changed webpack config to no longer use hot reload due to being deployed soon

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,16 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
 
 
 module.exports = {
     mode: 'development',
-    entry: [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        './src/scripts/main.js'
-    ],
+    entry: {
+        main: "./src/scripts/main.js",
+    },
     output: {
-        path: path.resolve('./assets/bundles/'),
-        filename: '[name].js',
-        publicPath: 'http://localhost:3000/assets/bundles/', // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
+        path: path.resolve('./assets/webpack_bundles/'),
+        filename: "[name].js"
     },
     resolve: {
         extensions: [".js", ".ts", ".css", ".scss", ".tsx"],
@@ -41,8 +37,6 @@ module.exports = {
     },
     plugins: [
         new BundleTracker({filename: './webpack-stats.json'}),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(), // don't reload if there is an error
         new MiniCssExtractPlugin({filename: '[name].css'}),
     ]
 
