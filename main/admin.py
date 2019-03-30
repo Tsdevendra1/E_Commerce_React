488a77bf1ab784d72672e8de5a2c6c75be4eb4ff
tsdev <tsdevendra1@gmail.com>
2019-03-30 16:43:05 +0000

    - Tested AWS S3 Uploaded, working on this branch - Added image model to admin console

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from main.models import Product, ProductCategories
+from main.models import Product, ProductCategories, Image
 
 admin.site.register(Product)
+admin.site.register(Image)
 admin.site.register(ProductCategories)
